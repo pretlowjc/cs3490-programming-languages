a453c72c8af2131d3b5b9933ca015ef547450685
pretlowjc <justin.pretlow13@gmail.com>
2023-09-21 17:23:18 -0400

    Adding basic functions and learning its structure.

--- practice/haskell-syntax.hs
+++ practice/haskell-syntax.hs
@@ -1,3 +1,13 @@
 {-
 	This file is used for practicing and learning Haskell and its syntax.
 -}
+
+-- Functions 
+addOne :: Int -> Int
+addOne int = int + 1
+
+timesThree :: Int -> Int
+timesThree x = x * 3
+
+sayHello :: String -> String
+sayHello target = "Hello, " ++ target ++ "!"
