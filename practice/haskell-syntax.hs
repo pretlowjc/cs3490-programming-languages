619e61d31a077cfcb761b6cccc6d4d9b95a1cb7c
pretlowjc <justin.pretlow13@gmail.com>
2023-09-21 17:44:22 -0400

    Added code example and explanation of multiple argument functions.

--- practice/haskell-syntax.hs
+++ practice/haskell-syntax.hs
@@ -11,3 +11,21 @@ timesThree x = x * 3
 
 sayHello :: String -> String
 sayHello target = "Hello, " ++ target ++ "!"
+
+-- Multiple Argument Functions
+add :: Int -> Int -> Int -- 1
+add first second = first + second -- 2
+
+three :: Int 
+three = add 1 2 -- 3
+
+{- 
+1 We don’t need parenthesis around the output because the -> operator is 
+right-associative. This means that is it interpreted as, Int -> (Int -> Int). 
+
+2 The process of supplying less than the full set of arguments is called currying, 
+and is used very frequently in Haskell.
+
+3 We don’t need parenthesis around add 1, as they are implied. Function application 
+is left-associative. For example, the expression a b c d is interpreted as ((a b) c) d.
+-}
